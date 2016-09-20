83d9291052f1d1d04ebb84bb625037780fe1b374
Rainer Mühr <rainer.muehr@framefield.com>
2016-09-20 16:08:01 +0200

    Some tweaks to route paths and styling by pixtur

--- web-ui/client/routes.js
+++ web-ui/client/routes.js
@@ -6,27 +6,29 @@ import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 Planned routes are...
 
 /welcome
-/browse/_orgaId/
-/browse/_orgaId/_sourceId/
+/browse/:_orgaId/
+/browse/:_orgaId/:_sourceId/
 
 /browse/sources/
-/browse/sources/_sourceId/edit
-/browse/organizations/_orgaId/
-/browse/organizations/_orgaId/sources ???
+/browse/sources/:_sourceId/edit
+/browse/organizations/:_orgaId/
+/browse/organizations/:_orgaId/sources ???
+
+/ <- dashboard
 
 /manage/organizations/create
-/manage/organizations/_orgaId_    <- includes link to delete
-/manage/organizations/_orgaId_/edit
+/manage/organizations/:_orgaId_    <- includes link to delete
+/manage/organizations/:_orgaId_/edit
 
-/manage/organizations/_orgaId/sources/create
+/manage/organizations/:_orgaId/sources/create
 
-/manage/sources/_sourceId
+/manage/sources/:_sourceId
 
-/manage/sources/_sourceId/format
-/manage/sources/_sourceId/settings
-/manage/sources/_sourceId/imports/_importId
-/manage/sources/_sourceId/imports/_importId
-/manage/sources/_sourceId/access
+/manage/sources/:_sourceId/format
+/manage/sources/:_sourceId/settings
+/manage/sources/:_sourceId/imports/:_importId
+/manage/sources/:_sourceId/imports/:_importId
+/manage/sources/:_sourceId/access
 
 /help
 /signup
@@ -59,7 +61,9 @@ FlowRouter.route('/', {
         main: 'page_dashboard',
         header_navigation_list: 'dashboard_header_navigation' });
     } else {
-      BlazeLayout.render('app_layout_start_page', { main: 'page_start' });
+      BlazeLayout.render('app_layout_start_page', { 
+        main: 'page_start',
+      });
     }
   },
 });
