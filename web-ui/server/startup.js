8c777590167a79b7d54286973f774ae9ed9be2a3
Thomas Mann <thomas.mann@framefield.com>
2016-09-27 20:01:42 +0200

    Added browser-policy to fix rendering of map on staging; removed some commented code

--- web-ui/server/startup.js
+++ web-ui/server/startup.js
@@ -2,3 +2,6 @@ import { Meteor } from 'meteor/meteor';
 import { setupHTTPAuthentication } from '/imports/startup/server/http-basic-auth';
 
 Meteor.startup(setupHTTPAuthentication);
+
+BrowserPolicy.content.allowOriginForAll('api.mapbox.com');
+BrowserPolicy.content.allowOriginForAll('npmcdn.com');
