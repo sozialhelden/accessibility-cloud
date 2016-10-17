faba0abd084f40c478b7bcd5a3d8d59cf40fb8a6
Sebastian Felix Zappe <hypo@me.com>
2016-10-17 14:40:27 +0200

    Make layout template helpers / events DRY

---
+++ client/_layouts/app-layout-startpage.js
@@ -0,0 +1,11 @@
+import './app-body.html';
+
+import { Template } from 'meteor/templating';
+
+import { helpers, events, onCreated } from './helpers';
+
+import './app-layout-startpage.html';
+
+Template.app_layout_start_page.onCreated(onCreated);
+Template.app_layout_start_page.helpers(helpers);
+Template.app_layout_start_page.events(events);
