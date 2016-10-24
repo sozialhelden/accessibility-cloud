f52a58be40abfcc9f39ed0720a5a0f71b2da0a2f
Sebastian Felix Zappe <hypo@me.com>
2016-10-24 12:40:56 +0200

    Sort source imports by timestamp, newest first

--- both/api/source-imports/server/publications.js
+++ both/api/source-imports/server/publications.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import { SourceImports } from '../source-imports.js';
 
-const options = { fields: SourceImports.publicFields };
+const options = { fields: SourceImports.publicFields, sort: { startTimestamp: -1 } };
 Meteor.publish('sourceImports.public', () => SourceImports.find({}, options));
