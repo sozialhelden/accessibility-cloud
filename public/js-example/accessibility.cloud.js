5a56511e9c74663052f9b0c551ed442593d4457a
Sebastian Felix Zappe <hypo@me.com>
2016-10-26 01:39:36 +0200

    Add templates to JSON API example app

--- public/js-example/accessibility.cloud.js
+++ public/js-example/accessibility.cloud.js
@@ -1,23 +1,71 @@
-AccessibilityCloud = {
-  getPlacesAround: function getPlacesAround(parameters) {
-    return $.ajax({
-      dataType: 'json',
-      url: 'http://localhost:3000/place-infos',
-      data: parameters,
-      headers: {
-        Accept: 'application/json',
-        'X-Token': this.token,
-      },
-    });
-  },
-
-  renderPlaces: function renderPlaces(element, parameters) {
-    this.getPlacesAround(parameters)
-      .done(function handleResponse(response) {
-        $(element).text(JSON.stringify(response.features));
-      })
-      .fail(function handleError(error) {
-        $(element).html('<div class="error">Could not load data: ' + error.message + '</div>');
+(function () {
+  window.AccessibilityCloud = {
+    getPlacesAround: function (parameters) {
+      return $.ajax({
+        dataType: 'json',
+        url: 'http://localhost:3000/place-infos',
+        data: parameters,
+        headers: {
+          Accept: 'application/json',
+          'X-Token': this.token,
+        },
       });
-  },
-};
+    },
+
+    resultsTemplate: function () {
+      // TODO: Finish + test aria accessibility attributes
+      // Template format: https://github.com/janl/mustache.js
+      // eslint-disable-next-line no-multi-str
+      return '<ul class="ac-result-list" role="treegrid"> \
+        {{#places}} \
+          <li class="ac-result" role="gridcell" aria-expanded="false"> \
+            {{#properties}} \
+              <img src="icons/categories/{{category}}.png" role="presentation"> \
+              <header class="ac-result-name" role="heading">{{name}}</header> \
+              <div class="ac-result-category">{{category}}</div> \
+              <a href="{{detailsURL}}" class="ac-result-link">{{sourceId}}</a> \
+              <div class="ac-result-distance">{{formattedDistance}}</div> \
+              <div class="ac-result-accessibility">Accessibility: {{formattedAccessibility}}</div> \
+            {{/properties}} \
+          </li> \
+        {{/places}} \
+      </ul>';
+    },
+
+    renderPlaces: function (element, places) {
+      // $(element).text(JSON.stringify(response.features));
+      var self = this;
+      if (!$(element).length) {
+        console.error('Could not render results, element not found.');
+      }
+      if (places && places.length) {
+        $(element).html(Mustache.render(self.resultsTemplate(), {
+          places: places,
+          formattedDistance: function () {
+            return Math.round(this.distance) + 'm';
+          },
+          formattedAccessibility: function () {
+            return JSON.stringify(this.accessibility);
+          },
+        }));
+      } else {
+        $(element).html('<div class="ac-no-results">No results.</div>');
+      }
+    },
+
+    loadAndRenderPlaces: function (element, parameters) {
+      var self = this;
+
+      return this.getPlacesAround(parameters)
+        .done(function handleResponse(response) {
+          self.renderPlaces(element, response.features);
+        })
+        .fail(function handleError(error) {
+          var message = (error && error.message) || 'No error message';
+          $(element)
+            .append('<div class="ac-error"></div>')
+            .text('Could not load data: ' + message);
+        });
+    },
+  };
+}());
