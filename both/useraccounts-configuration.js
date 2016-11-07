63341ea7dec4f2a9be0a12ec5641f74ec806ca6c
Sebastian Felix Zappe <hypo@me.com>
2016-11-07 12:28:13 +0100

    Show form field errors next to form fields

--- both/useraccounts-configuration.js
+++ both/useraccounts-configuration.js
@@ -22,6 +22,11 @@ AccountsTemplates.configure({
   defaultLayout: 'App_body',
   defaultContentRegion: 'main',
   defaultLayoutRegions: {},
+  negativeFeedback: false,
+  negativeValidation: true,
+  positiveValidation: true,
+  positiveFeedback: true,
+  showValidating: true,
 });
 
 AccountsTemplates.configureRoute('signIn', {
@@ -44,7 +49,9 @@ AccountsTemplates.configureRoute('resetPwd', {
 AccountsTemplates.addField({
   _id: 'tocForUserAccepted',
   type: 'checkbox',
-  displayName: 'I have read and understood the terms and conditions',
+  displayName: 'I have read and understood the Terms and Conditions',
+  errStr: 'You must agree to the Terms and Conditions.',
+  required: true,
   template: 'customCheckboxTemplate',
 
   func(value) {
