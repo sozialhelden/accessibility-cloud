22585ac48abe76bd371975c8a247aa0b9edbd840
Sebastian Felix Zappe <hypo@me.com>
2017-02-09 18:16:53 +0100

    Fix a JS error when MultiHTTPDownload has no parameters.

--- client/pages/sources/stream-chain/components/multi-http-download.js
+++ client/pages/sources/stream-chain/components/multi-http-download.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 
 Template.sources_stream_chain_MultiHTTPDownload.helpers({
   curlString() {
-    const headers = this.parameters.headers;
+    const headers = this.parameters && this.parameters.headers || {};
     const headerParameters = Object.keys(headers)
       .map(key => `-H '${key}: ${headers[key]}'`)
       .join(' ');
