d88de8046ef9617305f12ba5ad9df827fb794a78
Antonio D'Ettole <codazzo@gmail.com>
2017-02-10 11:49:02 +0100

    Don't use i18n for server log messages
    Addresses [1]
    
    [1] https://github.com/sozialhelden/accessibility-cloud/pull/15#discussion_r100351218

--- both/api/source-access-requests/server/methods/ignore.js
+++ both/api/source-access-requests/server/methods/ignore.js
@@ -3,7 +3,6 @@ import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Sources } from '/both/api/sources/sources.js';
 import { OrganizationMembers } from '/both/api/organization-members/organization-members.js';
 import { SourceAccessRequests } from '../../source-access-requests.js';
-import { TAPi18n } from 'meteor/tap:i18n';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { ignoreAccessRequest } from '../invitations';
 
@@ -17,17 +16,17 @@ export const approve = new ValidatedMethod({
   }).validator(),
   run({ requestId }) {
     if (!this.userId) {
-      throw new Meteor.Error(401, TAPi18n.__('Please log in first.'));
+      throw new Meteor.Error(401, 'Please log in first.');
     }
 
     const sourceAccessRequest = SourceAccessRequests.findOne(requestId);
     if (!sourceAccessRequest) {
-      throw new Meteor.Error(404, TAPi18n.__('Access Request not found'));
+      throw new Meteor.Error(404, 'Access Request not found');
     }
 
     const source = Sources.findOne(sourceAccessRequest.sourceId);
     if (!source) {
-      throw new Meteor.Error(404, TAPi18n.__('Source ${sourceAccessRequest.sourceId} not found'));
+      throw new Meteor.Error(404, 'Source ${sourceAccessRequest.sourceId} not found');
     }
 
     const approverOrganizationId = source.organizationId;
@@ -38,8 +37,7 @@ export const approve = new ValidatedMethod({
     }));
 
     if (!isOrganizationMember) {
-      throw new Meteor.Error(403,
-        TAPi18n.__('You cannot approve access requests for this source.'));
+      throw new Meteor.Error(403, 'You cannot approve access requests for this source.');
     }
 
     const {
@@ -57,7 +55,7 @@ export const approve = new ValidatedMethod({
     } catch (e) {
       const message = `The request could not be ignored because of an error.\n ${e.message}`;
 
-      throw new Meteor.Error(500, TAPi18n.__(message));
+      throw new Meteor.Error(500, message);
     }
   },
 });
