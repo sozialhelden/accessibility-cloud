39f65fd869848c547096040759315f71242bde44
Antonio D'Ettole <codazzo@gmail.com>
2017-02-13 18:57:21 +0100

    Use assets API to read libraries used by TransformData streams

--- both/api/sources/server/stream-chain/stream-types/transform-data/get-vm-context.js
+++ both/api/sources/server/stream-chain/stream-types/transform-data/get-vm-context.js
@@ -1,22 +1,17 @@
-import fs from 'fs';
+/* global Assets: true */
+
 import vm from 'vm';
-import path from 'path';
 import getCategories from './get-categories';
 import getHelpersContent from './get-helpers-content';
 
-const LIBS_DIR_PATH = path.join(process.env.PWD, __dirname, 'libs');
+const AVAILABLE_LIBS = [
+  'lodash.min.js',
+];
 
-const getLibsContent = () => {
-  console.log(`LIBS_DIR_PATH: ${LIBS_DIR_PATH}`);
-  const libsFilePaths = fs.readdirSync(LIBS_DIR_PATH);
-
-  return libsFilePaths.reduce((content, fileName) => `
-    ${content}
-    ${fs.readFileSync(`${LIBS_DIR_PATH}/${fileName}`, {
-      encoding: 'utf8',
-    })}
-  `, '');
-};
+const getLibsContent = () =>
+  AVAILABLE_LIBS
+    .map(fileName => Assets.getText(`transform-libs/${fileName}`))
+    .join(';\n');
 
 export default function getVMContext() {
   const globalObject = Object.freeze({});
