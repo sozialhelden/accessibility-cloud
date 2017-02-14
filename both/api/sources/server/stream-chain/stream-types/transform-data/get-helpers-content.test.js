e88162d49558e774aab0435868968c44b9b4d962
Antonio D'Ettole <codazzo@gmail.com>
2017-02-14 14:13:28 +0100

    Add tests for getHelpersContent(), getVMContext()

---
+++ both/api/sources/server/stream-chain/stream-types/transform-data/get-helpers-content.test.js
@@ -0,0 +1,59 @@
+/* eslint-env mocha */
+
+import vm from 'vm';
+import _ from 'lodash';
+import { assert } from 'meteor/practicalmeteor:chai';
+import getHelpersContent from './get-helpers-content';
+
+describe('getHelpersContent', () => {
+  it('is a function', () => {
+    assert.isFunction(getHelpersContent);
+  });
+
+  describe('content', () => {
+    let content;
+
+    before(() => {
+      content = getHelpersContent();
+    });
+
+    it('is a string', () => {
+      assert.isString(content);
+    });
+  });
+
+  describe('evaluated object', () => {
+    let globalObject;
+    let context;
+    let helpersObj;
+
+    before(() => {
+      globalObject = {};
+      context = vm.createContext(globalObject);
+      const setupCode = `
+        'use strict';
+
+        var helpers = ${getHelpersContent()};
+      `;
+
+      vm.runInContext(setupCode, context);
+      helpersObj = globalObject.helpers;
+    });
+
+    const EXPECTED_METHOD_PATHS = [
+      'AXSMaps.estimateFlagFor',
+      'AXSMaps.estimateRatingFor',
+      'AXSMaps.fetchNameFromTags',
+      'AXSMaps.getCategoryFromList',
+      'AXSMaps.guessGeoPoint',
+      'OSM.fetchCategoryFromTags',
+      'extractNumber',
+    ];
+
+    EXPECTED_METHOD_PATHS.forEach(methodPath => {
+      it(`has a ${methodPath} method`, () => {
+        assert.isFunction(_.get(helpersObj, methodPath));
+      });
+    });
+  });
+});
