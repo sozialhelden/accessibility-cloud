a62abb71185c05dadf327e99a9c68de7f0c70d00
Antonio D'Ettole <codazzo@gmail.com>
2017-02-21 17:47:42 +0100

    Fix lint error by removing unused import

--- client/pages/sources/show/source-imports/imports.js
+++ client/pages/sources/show/source-imports/imports.js
@@ -1,11 +1,8 @@
-import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Sources } from '/both/api/sources/sources.js';
 import { SourceImports } from '/both/api/source-imports/source-imports.js';
 import { PlaceInfos } from '/both/api/place-infos/place-infos.js';
-
-
 import subsManager from '/client/lib/subs-manager';
 
 Template.sources_show_imports_page.onCreated(() => {
