056a608732ee8eebf99f94c2cb44502a2414dcb1
Antonio D'Ettole <codazzo@gmail.com>
2017-02-21 17:46:31 +0100

    Add Migration to move `streamChain` from sources to ImportFlows

---
+++ server/migrations.js
@@ -0,0 +1,29 @@
+/* global Migrations: true */
+
+import { Sources } from '/both/api/sources/sources';
+import { ImportFlows } from '/both/api/import-flows/import-flows';
+
+Migrations.add({
+  version: 1,
+  up() {
+    Sources.find({ streamChain: { $exists: true } }).forEach((source) => {
+      const sourceId = source._id;
+
+      ImportFlows.insert({
+        sourceId,
+        name: 'Default',
+        streams: source.streamChain,
+        createdAt: Date.now()
+      });
+      Sources.update({ _id: sourceId }, { $unset: { streamChain: true } });
+    });
+  },
+  down() {
+    ImportFlows.find({}).forEach((importFlow) => {
+      const sourceId = importFlow.sourceId;
+
+      Sources.update({ _id: sourceId }, { $set: { streamChain: importFlow.streams } });
+      ImportFlows.remove({ _id: importFlow._id });
+    });
+  },
+});
