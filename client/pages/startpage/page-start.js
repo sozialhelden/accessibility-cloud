de66fcf5306433f81e5d334bc764613a88f5c0d0
Sebastian Felix Zappe <hypo@me.com>
2017-02-21 15:51:01 +0100

    Fix linter errors in page-start.js

--- client/pages/startpage/page-start.js
+++ client/pages/startpage/page-start.js
@@ -1,26 +1,26 @@
+/* globals L */
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { $ } from 'meteor/jquery';
 import subsManager from '/client/lib/subs-manager';
+import { GlobalStats } from '/both/api/global-stats/global-stats';
 
-Template.page_start.onCreated(function startPageOnCreated() {
+Template.page_start.onCreated(() => {
   subsManager.subscribe('organizations.public');
+  subsManager.subscribe('globalStats.public');
 });
 
-Template.page_start.onRendered(function startPageOnRendered() {
-  this.autorun(() => {
-  });
-
-  let map = L.map('mapid', {zoomControl:false});
+Template.page_start.onRendered(() => {
+  const map = L.map('mapid', { zoomControl: false });
   map.fitBounds(
-      [[ 45, -120],[-10,120]]
+      [[45, -120], [-10, 120]]
   );
 
-	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGl4dHVyIiwiYSI6ImNpc2tuMWx1eDAwNHQzMnBremRzNjBqcXIifQ.3jo3ZXnwCVxTkKaw0RPlDg', {
+  L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicGl4dHVyIiwiYSI6ImNpc2tuMWx1eDAwNHQzMnBremRzNjBqcXIifQ.3jo3ZXnwCVxTkKaw0RPlDg', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'accesssibility-cloud',
     accessToken: Meteor.settings.public.mapbox || 'your.mapbox.public.access.token',
-	}).addTo(map);
+  }).addTo(map);
 });
 
 Template.page_start.helpers({
