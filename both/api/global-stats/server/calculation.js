37a2e5810ed97ed4503e0c0c682772ed8b9501f3
Sebastian Felix Zappe <hypo@me.com>
2017-03-06 14:34:10 +0100

    Don’t include draft sources in start page stats anymore

--- both/api/global-stats/server/calculation.js
+++ both/api/global-stats/server/calculation.js
@@ -6,18 +6,34 @@ import { PlaceInfos } from '/both/api/place-infos/place-infos';
 import { Sources } from '/both/api/sources/sources';
 import { Organizations } from '/both/api/organizations/organizations';
 
-export function saveCount(collection) {
+export function saveCount({ collection, countName, selector = {} }) {
   check(collection, Mongo.Collection);
-  const value = collection.find().count();
-  GlobalStats.insert({ name: `${collection._name}.count`, date: new Date(), value });
+  const value = collection.find(selector).count();
+  const name = `${collection._name}${countName ? `.${countName}` : ''}.count`;
+  GlobalStats.insert({ name, date: new Date(), value });
   console.log(`${value} ${collection._name} in total.`);
 }
 
 export function calculateGlobalStats() {
+  const sourceIdsWithoutDrafts = Sources
+    .find({ isDraft: false }, { fields: { _id: 1 } })
+    .fetch()
+    .map(source => source._id);
+
   [
-    Meteor.users,
-    PlaceInfos,
-    Sources,
-    Organizations,
+    { collection: Meteor.users },
+    { collection: PlaceInfos },
+    { collection: Sources },
+    { collection: Organizations },
+    {
+      collection: PlaceInfos,
+      countName: 'withoutDrafts',
+      selector: { 'properties.sourceId': { $in: sourceIdsWithoutDrafts } },
+    },
+    {
+      collection: Sources,
+      countName: 'withoutDrafts',
+      selector: { isDraft: false },
+    },
   ].forEach(saveCount);
 }
