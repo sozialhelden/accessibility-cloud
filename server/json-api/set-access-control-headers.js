25193d8bc3518e189a13a8ab623cfb15a58f6a1a
Sebastian Felix Zappe <hypo@me.com>
2017-04-03 17:40:15 +0200

    Fix CORS headers for supplying tokens via header

--- server/json-api/set-access-control-headers.js
+++ server/json-api/set-access-control-headers.js
@@ -4,6 +4,6 @@ export function setAccessControlHeaders(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', Object.keys(httpMethodHandlers));
   // eslint-disable-next-line max-len
-  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Encoding, Accept-Language, Cache-Control, Connection, Referer, User-Agent, X-Requested-With, X-Token');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Encoding, Accept-Language, Cache-Control, Connection, Referer, User-Agent, X-Requested-With, X-Token, X-App-Token, X-User-Token');
   res.setHeader('Access-Control-Max-Age', 86400);
 }
