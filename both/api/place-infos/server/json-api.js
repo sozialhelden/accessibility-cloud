d17f5005230b0cd6ac887f986ed2389fbf7f1842
Sebastian Felix Zappe <hypo@me.com>
2017-11-10 15:13:22 +0100

    Share GeoJSON wrapping model between entities

--- both/api/place-infos/server/json-api.js
+++ both/api/place-infos/server/json-api.js
@@ -7,76 +7,8 @@ import {
 } from '/server/i18n/ac-format-translations';
 import { Categories } from '/both/api/categories/categories';
 import { PlaceInfos } from '/both/api/place-infos/place-infos';
-import {
-  convertToGeoJSONFeature,
-  wrapAPIResponseAsGeoJSON,
-} from '../../shared/server/geo-json-conversion';
+import wrapAPIResponseAsGeoJSON from '../../shared/server/wrapAPIResponseAsGeoJSON';
 
-const helpers = {
-  getLocalizedCategory(locale) {
-    const category = Categories.findOne(this.properties.category);
-    if (!category) {
-      console.log(`Category ${this.properties.category} not found.`);
-      return '';
-    }
-    return category.getLocalizedId(locale);
-  },
-  getLocalizedAccessibility(locale) {
-    const result = _.cloneDeep(this.properties.accessibility);
-    const paths = pathsInObject(result);
-    paths.forEach(path => {
-      _.set(result, `${path}Localized`, getTranslationForAccessibilityAttributeName(path, locale));
-    });
-    return result;
-  },
-  getLocalizedName(locale) {
-    if (locale && !(typeof locale === 'string')) throw new Meteor.Error(422, 'Locale must be undefined or a string.');
-    if (!this.properties) return null;
-    if (typeof this.properties.name === 'string') return this.properties.name;
-    if (typeof this.properties.name === 'object') {
-      if (!locale) {
-        if (typeof this.properties.name.en_US === 'string') {
-          return this.properties.name.en_US;
-        }
-        if (typeof this.properties.name.en === 'string') {
-          return this.properties.name.en;
-        }
-        const firstAvailableLocale = Object.keys(this.properties.name)[0];
-        if (firstAvailableLocale && typeof this.properties.name[firstAvailableLocale] === 'string') {
-          return this.properties.name[firstAvailableLocale];
-        }
-      }
-      if (typeof this.properties.name[locale] === 'string') {
-        return this.properties.name[locale];
-      }
-      const localeWithoutCountry = locale.slice(0, 2);
-      if (typeof this.properties.name[localeWithoutCountry] === 'string') {
-        return this.properties.name[localeWithoutCountry];
-      }
-      if (typeof this.properties.name.en_US === 'string') {
-        return this.properties.name.en_US;
-      }
-      if (typeof this.properties.name.en === 'string') {
-        return this.properties.name.en;
-      }
-    }
-    return null;
-  },
-};
-
-
-// Convert a given plain MongoDB document (not transformed) into a GeoJSON feature
-PlaceInfos.convertToGeoJSONFeature = (doc, coordinatesForDistance, locale) => {
-  const convertedDocument = convertToGeoJSONFeature(doc, coordinatesForDistance, locale);
-  if (locale) {
-    Object.assign(convertedDocument.properties, {
-      localizedCategory: helpers.getLocalizedCategory.call(doc, locale),
-      accessibility: helpers.getLocalizedAccessibility.call(doc, locale),
-    });
-  }
-  convertedDocument.properties.name = helpers.getLocalizedName.call(doc, locale);
-  return convertedDocument;
-};
 
 PlaceInfos.wrapAPIResponse = wrapAPIResponseAsGeoJSON;
 
@@ -88,7 +20,5 @@ PlaceInfos.relationships = {
     },
   },
 };
-
-PlaceInfos.helpers(helpers);
 
 PlaceInfos.includePathsByDefault = ['source.license'];
