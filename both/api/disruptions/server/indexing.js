c93d701eaecae1d81907ea0d3a1ddae32ae3ee57
Sebastian Felix Zappe <hypo@me.com>
2017-11-15 22:42:12 +0100

    Add original ID indexes for equipment and disruptions

--- both/api/disruptions/server/indexing.js
+++ both/api/disruptions/server/indexing.js
@@ -9,7 +9,9 @@ Meteor.startup(() => {
   Disruptions._ensureIndex({ 'properties.placeSourceId': 1 });
   Disruptions._ensureIndex({ 'properties.equipmentSourceId': 1 });
   Disruptions._ensureIndex({ 'properties.placeInfoId': 1 });
+  Disruptions._ensureIndex({ 'properties.originalPlaceInfoId': 1 });
   Disruptions._ensureIndex({ 'properties.equipmentInfoId': 1 });
+  Disruptions._ensureIndex({ 'properties.originalEquipmentInfoId': 1 });
 
   console.log('Ensuring geospatial index for Disruptions...');
   Disruptions._ensureIndex({ geometry: '2dsphere' });
