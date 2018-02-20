beed295d27d67a574def8cc6c1ecaaff0dda9315
Sebastian Felix Zappe <hypo@me.com>
2018-02-20 17:28:21 +0100

    Fix a DB overload bugcaused by a missing index

--- both/api/source-imports/server/indexing.js
+++ both/api/source-imports/server/indexing.js
@@ -4,4 +4,5 @@ import { SourceImports } from '../source-imports';
 Meteor.startup(() => {
   SourceImports._ensureIndex({ sourceId: 1 });
   SourceImports._ensureIndex({ organizationId: 1 });
+  SourceImports._ensureIndex({ startTimestamp: 1 });
 });
