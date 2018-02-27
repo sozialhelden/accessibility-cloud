b85f694e6f57dda8f7963088566d610c2bf2250f
Sebastian Felix Zappe <hypo@me.com>
2018-02-27 14:16:48 +0100

    Fix ConsoleOutput stream to work correctly

--- both/api/sources/server/stream-chain/stream-types/console-output.js
+++ both/api/sources/server/stream-chain/stream-types/console-output.js
@@ -1,10 +1,23 @@
-// import { Npm } from 'meteor/npm';
-const zstreams = Npm.require('zstreams');
+import util from 'util';
+
+const { Transform } = Npm.require('zstreams');
 
 export default class ConsoleOutput {
   constructor() {
-    this.stream = new zstreams.ConsoleLogStream({
+    this.stream = new Transform({
       writableObjectMode: true,
+      readableObjectMode: true,
+      transform(chunk, encoding, callback) {
+        console.log(util.inspect(chunk, { depth: 10, colors: true }));
+        callback(null, chunk);
+      },
+      flush(callback) {
+        callback();
+      },
+    });
+
+    this.stream.on('pipe', source => {
+      source.on('length', length => this.stream.emit('length', length));
     });
   }
 
