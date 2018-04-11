000c41d60a3e1c1f7714eefe2dc86fe2417c1bd1
Sebastian Felix Zappe <hypo@me.com>
2018-04-11 14:13:46 +0200

    Don’t set equipment to active after sensor reboot
    …and add fastly purging in debug mode

--- both/api/equipment-status-reports/server/update-equipment.js
+++ both/api/equipment-status-reports/server/update-equipment.js
@@ -2,18 +2,21 @@ import set from 'lodash/set';
 import { EquipmentInfos } from '../../equipment-infos/equipment-infos';
 import { PlaceInfos } from '../../place-infos/place-infos';
 import { EquipmentStatusReports } from '../equipment-status-reports';
+import purgeOnFastly from '../../../../server/purgeOnFastly';
+import isEmpty from 'lodash/isEmpty';
 
 const MaximalAllowedInactivityInSeconds = 60 * 60;
 
 
 export default function updateEquipmentWithStatusReport(report) {
-  const isActive = report.timeSinceLastActivityInSeconds < MaximalAllowedInactivityInSeconds;
   console.log('inactive since', report.timeSinceLastActivityInSeconds, 'max allowed', MaximalAllowedInactivityInSeconds);
   const lastUpdate = new Date().toISOString();
   const modifier = {};
-  if (isActive || report.isWorking) {
+  if (report.isWorking) {
     set(modifier, ['$set', 'properties.isWorking'], true);
     set(modifier, ['$set', 'properties.lastUpdate'], lastUpdate);
+  } else if (typeof report.timeSinceLastActivityInSeconds === 'number' && report.timeSinceLastActivityInSeconds < MaximalAllowedInactivityInSeconds) {
+    set(modifier, ['$set', 'properties.isWorking'], true);
   } else if (typeof report.isWorking === 'undefined') {
     set(modifier, ['$unset', 'properties.isWorking'], true);
   } else {
@@ -21,17 +24,21 @@ export default function updateEquipmentWithStatusReport(report) {
     set(modifier, ['$set', 'properties.lastUpdate'], lastUpdate);
   }
   const id = report.equipmentInfoId;
-
   console.log('Updating equipment', id, modifier);
-  EquipmentInfos.update(id, modifier);
+  if (!isEmpty(modifier)) {
+    EquipmentInfos.update(id, modifier);
+  }
   const equipmentInfo = EquipmentInfos.findOne(id, { transform: null, fields: { statusReportToken: false, 'properties.originalData': false } });
   if (!equipmentInfo) return;
-  if (!equipmentInfo.placeInfoId) return;
-  PlaceInfos.update(equipmentInfo.placeInfoId, {
+  if (!equipmentInfo.properties) return;
+  if (!equipmentInfo.properties.placeInfoId) return;
+  const placeInfoId = equipmentInfo.properties.placeInfoId;
+  PlaceInfos.update(placeInfoId, {
     $set: {
       [`properties.equipmentInfos.${equipmentInfo._id}`]: equipmentInfo,
     },
   });
+  // purgeOnFastly([placeInfoId, equipmentInfo._id]);
 }
 
 EquipmentStatusReports.afterInsertViaAPI = (doc) => {
---
+++ server/purgeOnFastly.js
@@ -0,0 +1,55 @@
+// @flow
+import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
+import { check } from 'meteor/check';
+import { isAdmin } from '../both/lib/is-admin';
+
+export default function purgeOnFastly(surrogateKeys: string[]): ?HTTP.HTTPResponse {
+  if (!surrogateKeys) return null;
+  if (!surrogateKeys.length) return null;
+
+  const { fastlyKey, serviceId } = Meteor.settings.fastly;
+
+  if (!fastlyKey) {
+    console.log('Did not purge', surrogateKeys, 'on Fastly because no `fastlyKey` is set.');
+    return null;
+  }
+
+  if (!serviceId) {
+    console.log('Did not purge', surrogateKeys, 'on Fastly because no `serviceId` is set.');
+    return null;
+  }
+
+  const url = `https://api.fastly.com/service/${serviceId}/purge`;
+  const options = {
+    headers: {
+      'Fastly-Key': fastlyKey,
+      Accept: 'application/json',
+      'Surrogate-Key': surrogateKeys.join(' '),
+    },
+  };
+
+  const result = HTTP.post(url, options);
+  console.log('Sent purge request for', surrogateKeys, 'to Fastly, result:', result);
+
+  return result;
+}
+
+
+Meteor.methods({
+  'fastly.purge'(keys) {
+    if (!this.userId) {
+      throw new Meteor.Error(401, 'Please log in first.');
+    }
+
+    if (!isAdmin(this.userId)) {
+      throw new Meteor.Error(403, 'Not authorized');
+    }
+
+    this.unblock();
+
+    check(keys, [String]);
+
+    return purgeOnFastly(keys);
+  },
+});
