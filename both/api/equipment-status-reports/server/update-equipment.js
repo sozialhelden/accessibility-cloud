9d471db7ecf5e21102bc106750323e473bcb45f7
Sebastian Felix Zappe <hypo@me.com>
2018-04-11 16:58:40 +0200

    Further improvements on isWorking flag behavior

--- both/api/equipment-status-reports/server/update-equipment.js
+++ both/api/equipment-status-reports/server/update-equipment.js
@@ -16,43 +16,45 @@ export default function updateEquipmentWithStatusReport(report) {
     // `true` means the sensor sensed that the elevator had a ride. `false` can be sent in case of
     // a power outage (in this case they might be unplugged for maintenance).
     // This is saved in the DB as it is explicit knowledge.
-
+    console.log('Got explicit isWorking flag', report.isWorking, 'from sensor.');
     set(modifier, ['$set', 'properties.lastUpdate'], lastUpdate);
     set(modifier, ['$set', 'properties.isWorking'], report.isWorking);
   } else if (typeof report.timeSinceLastActivityInSeconds === 'number' && report.timeSinceLastActivityInSeconds >= 0) {
     // The sensor has not been rebooted and still knows the last ride time...
+    console.log('Sensor not rebooted since last activity.');
     if (report.timeSinceLastActivityInSeconds < MaximalAllowedInactivityInSeconds) {
       // ...which was not long ago! -> Set elevator to working.
       set(modifier, ['$set', 'properties.isWorking'], true);
-    } else if (typeof report.isWorking === 'undefined') {
-      // ...the last ride was too long ago and the sensor does not know if the elevator works.
-      // -> Unset isWorking flag.
+    } else {
+      // ...the last ride was too long ago -> unset isWorking flag.
       set(modifier, ['$unset', 'properties.isWorking'], true);
     }
+  } else if (typeof report.timeSinceLastActivityInSeconds === 'number' && report.timeSinceLastActivityInSeconds < 0) {
+    // Sensor was rebooted.
   }
 
   const id = report.equipmentInfoId;
   console.log('Updating equipment', id, modifier);
   if (!isEmpty(modifier)) {
     EquipmentInfos.update(id, modifier);
-  }
-  const equipmentInfo = EquipmentInfos.findOne(id, { transform: null, fields: { statusReportToken: false, 'properties.originalData': false } });
-  if (!equipmentInfo) return;
-  if (!equipmentInfo.properties) return;
-  if (!equipmentInfo.properties.placeInfoId) return;
-  const placeInfoId = equipmentInfo.properties.placeInfoId;
-  const cacheUpdateModifier = {
-    $set: {
-      [`properties.equipmentInfos.${equipmentInfo._id}`]: equipmentInfo,
-    },
-  };
-  if (typeof equipmentInfo.properties.isWorking === 'undefined') {
-    cacheUpdateModifier.$unset = {
-      [`properties.equipmentInfos.${equipmentInfo._id}.properties.isWorking`]: true,
+    const equipmentInfo = EquipmentInfos.findOne(id, { transform: null, fields: { statusReportToken: false, 'properties.originalData': false } });
+    if (!equipmentInfo) return;
+    if (!equipmentInfo.properties) return;
+    if (!equipmentInfo.properties.placeInfoId) return;
+    const placeInfoId = equipmentInfo.properties.placeInfoId;
+    const cacheUpdateModifier = {
+      $set: {
+        [`properties.equipmentInfos.${equipmentInfo._id}`]: equipmentInfo,
+      },
     };
+    if (typeof equipmentInfo.properties.isWorking === 'undefined') {
+      cacheUpdateModifier.$unset = {
+        [`properties.equipmentInfos.${equipmentInfo._id}.properties.isWorking`]: true,
+      };
+    }
+    PlaceInfos.update(placeInfoId, cacheUpdateModifier);
+    purgeOnFastly([placeInfoId, equipmentInfo._id]);
   }
-  PlaceInfos.update(placeInfoId, cacheUpdateModifier);
-  purgeOnFastly([placeInfoId, equipmentInfo._id]);
 }
 
 EquipmentStatusReports.afterInsertViaAPI = (doc) => {
