8627ed8b34b52338bad6bfe75c0363cbb62c9063
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2018-07-05 11:14:01 +0200

    Improved access control headers for pre-flight requests
    - Allow overriding methods in access-control-headers
    - added content type header

--- server/json-api/set-access-control-headers.js
+++ server/json-api/set-access-control-headers.js
@@ -1,9 +1,9 @@
 import httpMethodHandlers from './http-methods';
 
-export function setAccessControlHeaders(res) {
+export function setAccessControlHeaders(res, methodsArray = null) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', Object.keys(httpMethodHandlers));
+  res.setHeader('Access-Control-Allow-Methods', methodsArray || Object.keys(httpMethodHandlers));
   // eslint-disable-next-line max-len
-  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Encoding, Accept-Language, Cache-Control, Connection, Referer, User-Agent, X-Requested-With, X-Token, X-App-Token, X-User-Token');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Encoding, Accept-Language, Cache-Control, Connection, Referer, User-Agent, X-Requested-With, X-Token, X-App-Token, X-User-Token, Content-Type');
   res.setHeader('Access-Control-Max-Age', 86400);
 }
