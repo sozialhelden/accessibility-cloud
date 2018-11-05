795468900238f8bb074068120f8cce1cd6b04f3c
Sebastian Felix Zappe <hypo@me.com>
2018-11-05 12:00:07 +0100

    Remove `returnNullIfFunctionThrows404` from new i18n implementation

--- server/i18n/getSupportedLocales.ts
+++ server/i18n/getSupportedLocales.ts
@@ -1,9 +1,8 @@
 import { union } from 'lodash';
 import getLocales from "./getLocales";
-import returnNullIfFunctionThrows404 from "./returnNullIfFunctionThrows404";
 
 export default function getSupportedLocales(resourceSlug: string, defaultLocale: string) {
-  const remoteLocales = returnNullIfFunctionThrows404(getLocales)(resourceSlug) || [];
+  const remoteLocales = getLocales(resourceSlug) || [];
   if (!remoteLocales.length) {
     console.log('Resource not existing on transifex yet, no remote locales found.');
   }
