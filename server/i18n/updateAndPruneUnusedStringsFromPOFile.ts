9173ba3e9c004255631e176eb15c2f38b53c4dbe
Sebastian Felix Zappe <hypo@me.com>
2018-11-18 03:40:33 +0100

    Fix several more transifex issues that happened in app link i18n sync.
    - Improve logging for transifex syncing
    - Fix mixed up `en_US`/`en` locales that happened because `en` resource on transifex had `en_US` in its header
    - Fix adding local translations to transifex if they don’t exist there yet
    - Fix cases where local translation was not set correctly if missing

--- server/i18n/updateAndPruneUnusedStringsFromPOFile.ts
+++ server/i18n/updateAndPruneUnusedStringsFromPOFile.ts
@@ -4,62 +4,69 @@ import {
   GetLocalTranslationFn,
   SetLocalTranslationFn,
   MsgidsToTranslationDescriptors,
+  POFile,
 } from './i18nTypes';
-import { POFile } from './i18nTypes';
 
-// - Write new translations from transifex into local docs
+// - Pull new translations from transifex into local docs
 // - clone given PO file, remove translations from clone that are not in local docs anymore
-// - return the modified PO file copy
+// - return the modified PO file copy for pushing
 export default function updateAndPruneUnusedStringsFromPOFile({
   context,
   getLocalTranslation,
+  locale,
   msgidsToTranslationDescriptors,
   poFile,
   setLocalTranslation,
 }: {
   context: string;
   getLocalTranslation: GetLocalTranslationFn;
+  locale: string,
   msgidsToTranslationDescriptors: MsgidsToTranslationDescriptors;
   poFile: POFile;
   setLocalTranslation: SetLocalTranslationFn;
 }) {
-  const locale = poFile.headers.language;
+  console.log('Pulling new translations into local docs, merging, pruning...');
   const newPOFile = cloneDeep(poFile);
   newPOFile.translations[context] = newPOFile.translations[context] || {};
   const newPOFileTranslations = newPOFile.translations[context];
   const msgids = Object.keys(msgidsToTranslationDescriptors);
   let updatedLocalStringsCount = 0;
-  msgids.forEach(msgid => {
+  let updatedRemoteStringsCount = 0;
+  msgids.forEach((msgid) => {
     const poFileTranslation = newPOFileTranslations[msgid];
     const { propertyName, doc } = msgidsToTranslationDescriptors[msgid];
-    const existingTranslation = getLocalTranslation({
-      propertyName,
+    const localMsgstr = getLocalTranslation({
       doc,
+      propertyName,
       locale,
     });
-    if (poFileTranslation) {
+    if (locale !== 'en' && poFileTranslation && poFileTranslation.msgstr.filter(str => str.length).length) {
       // overwrite existing local translation with data from PO file if necessary
       const msgstr = poFileTranslation.msgstr[0];
-      if (msgstr && existingTranslation !== msgstr) {
-        console.log(`Updating local string ‘${msgstr}’...`);
-        updatedLocalStringsCount++;
+      if (msgstr && localMsgstr !== msgstr) {
+        console.log(`Local string ‘${localMsgstr}’ is different from remote string ‘${poFileTranslation.msgstr}’...`);
+        updatedLocalStringsCount += 1;
         setLocalTranslation({ doc, locale, msgstr, propertyName });
       }
-    } else {
+    } else if (localMsgstr) {
       // add existing local translation to PO file
-      const isDefaultLocale = locale === 'en_US';
+      updatedRemoteStringsCount += 1;
       newPOFileTranslations[msgid] = {
         msgid,
-        msgstr: [
-          existingTranslation || (isDefaultLocale ? humanize(msgid) : ''),
-        ],
+        msgstr: [localMsgstr || ''],
       };
     }
   });
-  console.log(`Updated ${updatedLocalStringsCount} local strings.`);
+
+  if (updatedLocalStringsCount) {
+    console.log(`Updated ${updatedLocalStringsCount} local strings`);
+  }
+  if (updatedRemoteStringsCount) {
+    console.log(`About to push ${updatedRemoteStringsCount} new remote strings.`);
+  }
   const translatedMsgids = Object.keys(newPOFileTranslations);
   const msgidsToDelete = without(difference(translatedMsgids, msgids), '');
-  msgidsToDelete.forEach(msgid => {
+  msgidsToDelete.forEach((msgid) => {
     delete newPOFileTranslations[msgid];
   });
   console.log('Deleted msgids from synced PO file:', msgidsToDelete);
