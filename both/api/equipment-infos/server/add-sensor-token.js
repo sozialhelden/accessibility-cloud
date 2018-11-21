60a4ef5d0501c0eab772271605e95ee2091101eb
Sebastian Felix Zappe <hypo@me.com>
2018-11-22 00:15:46 +0100

    Do not automatically add sensor tokens for new equipment

--- both/api/equipment-infos/server/add-sensor-token.js
+++ both/api/equipment-infos/server/add-sensor-token.js
@@ -1,14 +1,16 @@
-import { Meteor } from 'meteor/meteor';
-import { Random } from 'meteor/random';
-import { EquipmentInfos } from '../equipment-infos';
+// Disabled for now for better performance
 
-Meteor.startup(() => {
-  EquipmentInfos.find({
-    statusReportToken: { $exists: false },
-  }).observeChanges({
-    added(id) {
-      console.log('Add secret status report token to equipment info', id);
-      EquipmentInfos.update(id, { $set: { statusReportToken: Random.secret(19) } });
-    },
-  });
-});
+// import { Meteor } from 'meteor/meteor';
+// import { Random } from 'meteor/random';
+// import { EquipmentInfos } from '../equipment-infos';
+
+// Meteor.startup(() => {
+//   EquipmentInfos.find({
+//     statusReportToken: { $exists: false },
+//   }).observeChanges({
+//     added(id) {
+//       console.log('Add secret status report token to equipment info', id);
+//       EquipmentInfos.update(id, { $set: { statusReportToken: Random.secret(19) } });
+//     },
+//   });
+// });
