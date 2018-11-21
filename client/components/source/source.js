86d1031a7cc987d9e64e49b76796d1d7a2d2099a
Sebastian Felix Zappe <hypo@me.com>
2018-11-22 00:16:03 +0100

    Don’t subscribe to last source import update on dashboard

--- client/components/source/source.js
+++ client/components/source/source.js
@@ -3,12 +3,6 @@ import { Template } from 'meteor/templating';
 import { Sources } from '/both/api/sources/sources';
 import subsManager from '/client/lib/subs-manager';
 
-Template.component_source.onRendered(function rendered() {
-  if (this.data.source) {
-    subsManager.subscribe('sourceImports.last.public', this.data.source._id);
-  }
-});
-
 Template.component_source.helpers({
   isVisible() {
     const source = Sources.findOne(this._id);
