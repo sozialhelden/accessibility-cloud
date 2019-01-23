1adbae2248ccb04af1615bb06b8566bb963d09d8
Sebastian Felix Zappe <hypo@me.com>
2019-01-23 15:35:25 +0100

    Use tile coordinate index for x/y/z requests by default

--- both/api/shared/tile-indexing/mapTileSelector.ts
+++ both/api/shared/tile-indexing/mapTileSelector.ts
@@ -1,11 +1,12 @@
 import { _ } from 'meteor/underscore';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { tile2GeoJSONPolygon } from './tile2GeoJSONPolygon';
+import { includes } from 'lodash';
 
 // Return a MongoDB document selector for a search by distance built
 // with query parameters from given request.
 export default function mapTileSelector(req: { query: Object }) {
-  const locationQuery = _.pick(req.query, 'x', 'y', 'z', 'useTileCoordinateIndex');
+  const locationQuery = _.pick(req.query, 'x', 'y', 'z', 'ignoreTileCoordinateIndex');
   // If no location parameter is given, just return an empty selector
   if (!(locationQuery.x || locationQuery.y || locationQuery.z)) {
     return {};
@@ -30,9 +31,10 @@ export default function mapTileSelector(req: { query: Object }) {
       max: 21,
       decimal: false,
     },
-    useTileCoordinateIndex: {
-      type: Boolean,
+    ignoreTileCoordinateIndex: {
+      type: String,
       optional: true,
+      allowedValues: ['true', 'false', '0', '1'],
     },
   });
   // Clean the data to remove whitespaces and have correct types
@@ -40,16 +42,16 @@ export default function mapTileSelector(req: { query: Object }) {
   // Throw ValidationError if something is wrong
   schema.validate(locationQuery);
 
-  if (locationQuery.useTileCoordinateIndex) {
+  if (includes(['true', '1'], locationQuery.ignoreTileCoordinateIndex)) {
     return {
-      [`tileCoordinates.${locationQuery.z}.x`]: locationQuery.x,
-      [`tileCoordinates.${locationQuery.z}.y`]: locationQuery.y,
+      geometry: {
+        $geoWithin: { $geometry: tile2GeoJSONPolygon(locationQuery) },
+      },
     };
   }
 
   return {
-    geometry: {
-      $geoWithin: { $geometry: tile2GeoJSONPolygon(locationQuery) },
-    },
+    [`tileCoordinates.${locationQuery.z}.x`]: locationQuery.x,
+    [`tileCoordinates.${locationQuery.z}.y`]: locationQuery.y,
   };
 }
