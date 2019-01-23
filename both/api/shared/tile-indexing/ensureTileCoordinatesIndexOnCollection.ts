f97435742c390c502ce4f21bd74d7741aedc164d
Sebastian Felix Zappe <hypo@me.com>
2019-01-23 16:59:13 +0100

    Fix tile index generation on start

--- both/api/shared/tile-indexing/ensureTileCoordinatesIndexOnCollection.ts
+++ both/api/shared/tile-indexing/ensureTileCoordinatesIndexOnCollection.ts
@@ -5,7 +5,14 @@ export default function ensureTileCoordinatesIndexOnCollection(collection: Mongo
   const collectionName = get(collection, '_name');
   console.log('Ensure tile coordinates indexing on', collectionName);
   collection._ensureIndex({ tileCoordinates: 1 });
-  Array.from({ length: 20 }).forEach((_, i) => {
-    collection._ensureIndex({ [`tileCoordinates.${i}.x`]: 1, [`tileCoordinates.${i}.y`]: 1 });
+  Array.from({ length: 22 }).forEach((_, i) => {
+    collection._ensureIndex(
+      {
+        [`tileCoordinates.${i}.x`]: 1,
+        [`tileCoordinates.${i}.y`]: 1,
+        'properties.sourceId': 1,
+      },
+      { background: true },
+    );
   });
 }
