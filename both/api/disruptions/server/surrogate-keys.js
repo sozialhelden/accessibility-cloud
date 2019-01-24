5ca66f4c4e93597c9b5f5f8c2f96f163790d033e
Sebastian Felix Zappe <hypo@me.com>
2019-01-24 17:00:16 +0100

    Add surrogate keys with tile coordinates to JSON API results

--- both/api/disruptions/server/surrogate-keys.js
+++ both/api/disruptions/server/surrogate-keys.js
@@ -1,10 +1,13 @@
 import get from 'lodash/get';
 import { Disruptions } from '../disruptions';
+import tileSurrogateKeysForFeature from '../../shared/tile-indexing/tileSurrogateKeysForFeature';
 
 Disruptions.surrogateKeysForDocument = placeInfo =>
   [
     get(placeInfo, ['properties', 'sourceId']),
     get(placeInfo, ['properties', 'sourceImportId']),
-  ].filter(Boolean);
+  ]
+  .filter(Boolean)
+  .concat(tileSurrogateKeysForFeature(placeInfo));
 
 Disruptions.maximalCacheTimeInSeconds = 60 * 60 * 24 * 7;
--- both/api/place-infos/server/surrogate-keys.js
+++ both/api/place-infos/server/surrogate-keys.js
@@ -1,10 +1,13 @@
 import get from 'lodash/get';
 import { PlaceInfos } from '../place-infos';
+import tileSurrogateKeysForFeature from '../../shared/tile-indexing/tileSurrogateKeysForFeature';
 
 PlaceInfos.surrogateKeysForDocument = placeInfo =>
   [
     get(placeInfo, ['properties', 'sourceId']),
     get(placeInfo, ['properties', 'sourceImportId']),
-  ].filter(Boolean);
+  ]
+  .filter(Boolean)
+  .concat(tileSurrogateKeysForFeature(placeInfo));
 
 PlaceInfos.maximalCacheTimeInSeconds = 60 * 60 * 24 * 7;
