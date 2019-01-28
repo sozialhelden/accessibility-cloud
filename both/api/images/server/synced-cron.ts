75d50e52ef0c7b53491e7ea4c52b15903be6ac30
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2019-01-28 13:24:55 +0100

    Added image moderation notification email

---
+++ both/api/images/server/synced-cron.ts
@@ -0,0 +1,32 @@
+import { SyncedCron } from 'meteor/percolate:synced-cron';
+import { Email } from 'meteor/email';
+
+import { Images, DefaultModerationFilter } from '../images';
+
+SyncedCron.add({
+  name: 'ImageModerationNotification',
+
+  schedule(parser) {
+    return parser.recur().on('08:00:00').time();
+  },
+
+  job() {
+    const imagesAwaitingModeration = Images.find(DefaultModerationFilter).count();
+
+    if (imagesAwaitingModeration > 0) {
+      Email.send({
+        from: 'support@accessibility.cloud',
+        to: 'support@accessibility.cloud',
+        subject: `${imagesAwaitingModeration} images awaiting moderation on accessibility.cloud`,
+        text: `Hi,
+
+there are currently ${imagesAwaitingModeration} images awaiting moderation.
+
+You can approve them here: ${Meteor.absoluteUrl('admin/images')}
+
+Regards,
+Your friendly notification robots from accessibility.cloud.`,
+      });
+    }
+  },
+});
