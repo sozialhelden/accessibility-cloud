1e400c58ceef857111a13a785ba173ddbba270a5
Sebastian Felix Zappe <hypo@me.com>
2019-05-06 16:41:47 +0200

    Fix tile coordinate generation for features

--- both/api/shared/tile-indexing/generateTileCoordinatesForFeature.ts
+++ both/api/shared/tile-indexing/generateTileCoordinatesForFeature.ts
@@ -1,11 +1,12 @@
 import { latlon2tile } from './latlon2tile';
+import { isArray } from 'lodash';
 
 export default function generateTileCoordinatesForFeature(feature: any) {
   if (!feature) return undefined;
   if (!feature.geometry) return undefined;
   if (feature.geometry.type !== 'Point') return undefined;
   if (!feature.geometry.coordinates) return undefined;
-  if (!(feature.geometry.coordinates instanceof Array)) return undefined;
+  if (!isArray(feature.geometry.coordinates)) return undefined;
   const result: { [z: number]: { x: number, y: number } } = {};
   const [lon, lat] = feature.geometry.coordinates;
   Array.from({ length: 23 }).forEach((_, z) => {
