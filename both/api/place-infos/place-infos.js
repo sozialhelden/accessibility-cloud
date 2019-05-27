85c01cc13c6a70b0d0868d63b05d1b4633cb4782
Sebastian Felix Zappe <hypo@me.com>
2019-05-27 15:23:54 +0200

    Add `images` has-many relation to `PlaceInfo`

--- both/api/place-infos/place-infos.js
+++ both/api/place-infos/place-infos.js
@@ -5,6 +5,7 @@ import { EquipmentInfos } from '../equipment-infos/equipment-infos';
 import { Sources } from '../sources/sources';
 import { Disruptions } from '../disruptions/disruptions';
 import convertToGeoJSONFeature from '../shared/convertToGeoJSONFeature';
+import { Images } from '../images/images';
 
 
 export const PlaceInfos = new Mongo.Collection('PlaceInfos');
@@ -34,6 +35,10 @@ PlaceInfos.relationships = {
     disruptions: {
       foreignCollection: Disruptions,
       foreignKey: 'properties.placeInfoId',
+    },
+    images: {
+      foreignCollection: Images,
+      foreignKey: 'objectId',
     },
   },
 };
