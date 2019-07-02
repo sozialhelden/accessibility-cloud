d792d835ef0c5eb7af44ad010f8b2294e0faf75e
Henrik Hinrichs <henrik.hinrichs@framefield.com>
2019-07-02 11:25:49 +0200

    Removed debugger

--- both/api/apps/server/privileges.js
+++ both/api/apps/server/privileges.js
@@ -44,7 +44,7 @@ Apps.visibleSelectorForAppId = () => ({});
 Apps.helpers({
   editableBy(userId) {
     check(userId, String);
-    debugger
+
     return userHasFullAccessToReferencedOrganization(userId, this);
   },
 });
