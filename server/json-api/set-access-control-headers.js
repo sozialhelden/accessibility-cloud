0a250d8f2c2412a96662e42281915e1fe3cf8052
Sebastian Felix Zappe <hypo@me.com>
2019-09-30 15:18:29 +0200

    Add `elastic-apm-traceparent` to allowed CORS headers

--- server/json-api/set-access-control-headers.js
+++ server/json-api/set-access-control-headers.js
@@ -4,6 +4,6 @@ export function setAccessControlHeaders(res, methodsArray = null) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', methodsArray || Object.keys(httpMethodHandlers));
   // eslint-disable-next-line max-len
-  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Encoding, Accept-Language, Cache-Control, Connection, Referer, User-Agent, X-Requested-With, X-Token, X-App-Token, X-User-Token, Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Accept, Accept-Encoding, Accept-Language, Cache-Control, Connection, Referer, User-Agent, X-Requested-With, X-Token, X-App-Token, X-User-Token, Content-Type, elastic-apm-traceparent');
   res.setHeader('Access-Control-Max-Age', 86400);
 }
