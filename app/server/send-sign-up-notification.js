bb1a57b89dc2455fae5886804ff2656cf54c2a18
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2020-11-27 13:26:43 +0100

    Auto-approve users on sign-up, send mail to developers

--- app/server/send-sign-up-notification.js
+++ app/server/send-sign-up-notification.js
@@ -1,27 +1,47 @@
 import { Accounts } from 'meteor/accounts-base';
-import { Meteor } from 'meteor/meteor';
 import { Email } from 'meteor/email';
-import { getDisplayedNameForUser } from '/both/lib/user-name';
 
 Accounts.onCreateUser((options, user) => {
   console.log('Send notification email for signup:', user);
   try {
-    const userName = getDisplayedNameForUser(user);
+    const email = user && user.emails && user.emails[0] && user.emails[0].address;
+
     Email.send({
       from: 'support@accessibility.cloud',
-      to: 'support@accessibility.cloud',
-      subject: `${userName} signed up on accessibility.cloud`,
+      to: email,
+      cc: 'support@accessibility.cloud',
+      subject: 'Your sign-up on accessibility.cloud',
       text: `Hi,
 
-      ${userName} just signed up on accessibility.cloud.
+I saw you signed up on http://accessibility.cloud. Great, thanks for your interest! I’m curious to learn what you plan to use it for!
 
-      You can approve them here: ${Meteor.absoluteUrl('admin/admin')}
+We are a non-profit organization running accessibility.cloud and provide this service for free to other nonprofits. If you plan to use this commercially, let’s talk.
 
-      Regards,
-      Your friendly notification robots from accessibility.cloud.`,
+To get you started, here is the technical documentation for our service:
+https://github.com/sozialhelden/accessibility-cloud/blob/master/README.md
+
+Commercial or not — let me know if we shall set up a call to help you make this most useful for you. :)
+
+Kind regards,
+Holger
+
+--
+
+Holger Dieterich
+Vorstand (Chairman)
+Wheelmap.org Mitgründer (Co-Founder)
+
+E-Mail: holger@sozialhelden.de
+Tel.: +49-178-1870577
+LinkedIn: https://www.linkedin.com/in/dieterich/
+Twitter: http://www.twitter.com/sozialhelden
+Facebook: http://www.facebook.com/sozialhelden
+
+SOZIALHELDEN e.V., Andreasstr. 10, 10243 Berlin, Germany
+`,
     });
   } catch (error) {
     console.error('Could not send sign-up notification email:', error, error.stack);
   }
-  return Object.assign({}, user, { isApproved: false });
+  return Object.assign({}, user, { isApproved: true });
 });
