9411d33c3b82566c838df10a38fc447d07374d92
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2021-04-12 16:48:48 +0200

    Don’t stop non-aborted streams on each container restart except they are old

--- app/server/mark-streams-as-stopped.js
+++ app/server/mark-streams-as-stopped.js
@@ -3,7 +3,7 @@ import { SourceImports } from '/both/api/source-imports/source-imports';
 import { Sources } from '/both/api/sources/sources';
 
 Meteor.startup(() => {
-  const selector = { streamChain: { $elemMatch: { 'progress.isFinished': { $exists: false } } } };
+  const selector = { startTimestamp: { $lt: (Date.now() - (7 * 24 * 60 * 60 * 1000)) }, streamChain: { $elemMatch: { 'progress.isFinished': { $exists: false } } } };
   const unfinishedSourceImports = SourceImports.find(selector).fetch();
   console.log('Found', unfinishedSourceImports.length, 'left over unfinished source imports.');
   unfinishedSourceImports.forEach(sourceImport => {
