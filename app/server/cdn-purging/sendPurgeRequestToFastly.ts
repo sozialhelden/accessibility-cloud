111807056ace26b5723f0de4645c0a2c14bcaa18
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2021-06-03 17:57:33 +0200

    Purge fastly content on /both/ ACv1 and v2 services

--- app/server/cdn-purging/sendPurgeRequestToFastly.ts
+++ app/server/cdn-purging/sendPurgeRequestToFastly.ts
@@ -5,28 +5,43 @@ export default function sendPurgeRequestToFastly(surrogateKeys: string[]) {
   if (!surrogateKeys.length) {
     return null;
   }
-  const { fastlyKey = null, serviceId = null } = Meteor.settings.fastly || {};
+  const { fastlyKey = null, serviceIds = null } = Meteor.settings.fastly || {};
   if (!fastlyKey) {
     console.log('Didn\'t purge', surrogateKeys.length, 'keys because no `fastlyKey` is set.');
     return null;
   }
-  if (!serviceId) {
+  if (!serviceIds) {
     console.log('Didn\'t purge', surrogateKeys, 'on Fastly because no `serviceId` is set.');
     return null;
   }
-  const url = `https://api.fastly.com/service/${serviceId}/purge`;
-  const options = {
-    headers: {
-      'Fastly-Key': fastlyKey,
-      Accept: 'application/json',
-      'Surrogate-Key': surrogateKeys.join(' '),
-    },
-  };
-  const result = HTTP.post(url, options);
-  if (result.statusCode === 200) {
-    console.log(`Purged ${surrogateKeys.length} keys on Fastly.`);
-  } else {
-    console.log('Error while processing purge request on Fastly, result:', result);
-  }
-  return result;
+
+  check(serviceIds, [String]);
+
+  const results = serviceIds.map(serviceId => {
+    const url = `https://api.fastly.com/service/${serviceId}/purge`;
+    const options = {
+      headers: {
+        'Fastly-Key': fastlyKey,
+        Accept: 'application/json',
+        'Surrogate-Key': surrogateKeys.join(' '),
+      },
+    };
+
+    const result = HTTP.post(url, options);
+    if (result.statusCode === 200) {
+      console.log('Purged', surrogateKeys, 'for service', serviceId, 'on Fastly.');
+    } else {
+      console.log(
+        'Error while processing purge request for',
+        surrogateKeys,
+        'on Fastly for service',
+        serviceId,
+        '- result:',
+        result
+      );
+    }
+    return result;
+  });
+
+  return results;
 }
