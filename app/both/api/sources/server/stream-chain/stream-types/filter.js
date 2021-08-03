66e77d9e51585cf9b1752d5aef7350a836c19b75
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2021-08-03 22:47:52 +0200

    Improve `Filter` stream
    - Add matched object count as debug info
    - Fix missing callback for non-matching input chunks. This fixes that stream chains using `Filter` were not ending if not all chunks matched the filter expression

--- app/both/api/sources/server/stream-chain/stream-types/filter.js
+++ app/both/api/sources/server/stream-chain/stream-types/filter.js
@@ -6,10 +6,12 @@ import { check, Match } from 'meteor/check';
 const { Transform } = Npm.require('zstreams');
 
 export default class Filter {
-  constructor({ path, expectedValue, negate }) {
+
+  constructor({ path, expectedValue, negate, onDebugInfo }) {
     check(path, String);
-    check(expectedValue, Match.Any());
     check(negate, Match.Optional(Boolean));
+
+    let numberOfMatchingDocuments = 0;
 
     this.stream = new Transform({
       writableObjectMode: true,
@@ -19,10 +21,16 @@ export default class Filter {
         const valueIsTrueish = !!value;
         const valueIsAsExpected = expectedValue ? isEqual(expectedValue, value) : valueIsTrueish;
         if (negate ? !valueIsAsExpected : valueIsAsExpected) {
+          numberOfMatchingDocuments += 1;
           callback(null, chunk);
+        } else {
+          callback();
         }
       },
       flush(callback) {
+        onDebugInfo({
+          numberOfMatchingDocuments,
+        });
         callback();
       },
     });
@@ -31,6 +39,7 @@ export default class Filter {
       source.on('length', length => this.stream.emit('length', length));
     });
   }
+
 
   dispose() {
     delete this.stream;
