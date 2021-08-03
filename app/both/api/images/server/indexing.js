edae7c111244fdcceeda2d1504a682bd669f3e4d
Sebastian Felix Zappe <sebastian@sozialhelden.de>
2021-08-03 22:59:02 +0200

    Index images by `sourceId` + `originalId`

--- app/both/api/images/server/indexing.js
+++ app/both/api/images/server/indexing.js
@@ -4,6 +4,7 @@ import { Images } from '../images';
 Meteor.startup(() => {
   // prevent re-uploading foreign images multiple times
   Images._ensureIndex({ originalId: 1 });
+  Images._ensureIndex({ sourceId: 1, originalId: 1 });
   // for rest api
   Images._ensureIndex({ objectId: 1, context: 1, isUploadedToS3: 1, moderationRequired: 1 });
   // throttling, filter by ip & order by time
